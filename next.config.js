7d763d1a8300cbe44190ed75d7fbc1c6725134cc
nedzen <nedzen.11@gmail.com>
2024-02-09 15:00:46 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const withImages = require('next-images');
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.symlinks = false;
+        return config;
+    },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ['mjml'],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/en',
+                permanent: true,
+            },
+        ];
+    },
+};
+
+module.exports = nextConfig;
+module.exports = withImages(nextConfig);
